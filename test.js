289cf919f73fd9e81e02edde3cd5730bd678d3aa
Caden Buckelew <buckelew@Cadens-MacBook-Air.local>
2022-12-18 22:42:46 -0800

    removed junk files

--- test.js
+++
@@ -1,9 +0,0 @@
-const cars = require("./server/carData.json");
-
-const origins = new Set();
-
-cars.forEach((car) => {
-  origins.add(car.origin.value);
-});
-
-console.log(Array.from(origins).join("\n"));
